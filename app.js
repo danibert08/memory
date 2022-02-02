dbdb3b002542eedc8a5dc6bdf60ca68d8a2ca5c3
BERTHIER <contact@daniel-dev.me>
2022-02-02 17:10:31 +0100

    Mise en place du jeu

---
+++ app.js
@@ -0,0 +1,27 @@
+// Afficher le plateau de jeu
+
+
+// Générer la position aléatoire des images
+
+// Masquer les images
+
+// Générer l'affichage de l'image 1 au click 
+
+// Générer l'affichage de l'image 2 au click
+
+// Créer la fonction de comparaison des images
+
+// Générer l'action si comparaison false
+
+// générer l'action si comparaison true
+
+// générer le chrono
+
+// Générer l'affichage des 5 meilleurs temps
+
+
+// Générer l'alert "Victoire"
+
+// Générer l'alert "perdu fin du jeu"
+
+// Créer la fonction rejouer
