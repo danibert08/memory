5d79b704ea9085ad6bd8375b00afda9222b91cd3
BERTHIER <contact@daniel-dev.me>
2022-02-02 19:18:27 +0100

    insert img

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
-// Afficher le plateau de jeu
+// Générer le plateau de jeu
 
+let grid = [
+      ['', 'b', 'b', 'b', '',''],
+      ['', '', '', '', '', '',''],
+      ['', '', '', '', '', '',''],
+      ['', '', '', '', '', '',''],
+      ['', '', '', '', '', '',''],
+      ['', '', '', '', '', '',''],
+ ];
+ 
 
+const gridHeaders = {
+    rows : [1,2,3,4,5,6],
+    columns : ['A','B','C','D','E','F']
+};
+
+function displayLine(line){
+    let lineToDisplay = " ";
+    for (let columnIndex= 0; columnIndex < line.length; columnIndex++){
+            let currentChar = line[columnIndex];
+            if(line[columnIndex] === ""){
+                lineToDisplay += '~';
+            }else{
+                lineToDisplay += currentChar;
+                
+            }
+           lineToDisplay += " ";     
+    }
+    return (lineToDisplay);
+};
+
+function displayGrid(){
+    console.log('  ' + gridHeaders.columns.join(' '));
+        for(let rowIndex=0; rowIndex<grid.length; rowIndex++){
+        
+       let row  =  gridHeaders.rows[rowIndex];
+       row += ' ' + displayLine(grid[rowIndex]);
+       console.log(row);
+    }
+};
+displayGrid();
 // Générer la position aléatoire des images
-
+let cell = document.getElementById('cell13')
+let img = document.createElement('img');
+img.src="./img/4.png"
+cell.appendChild(img);
 // Masquer les images
 
 // Générer l'affichage de l'image 1 au click 
@@ -18,7 +60,6 @@
 // générer le chrono
 
 // Générer l'affichage des 5 meilleurs temps
-
 
 // Générer l'alert "Victoire"
 
